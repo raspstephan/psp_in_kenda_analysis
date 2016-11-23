bcbebe91ec38488d8f691af96dde89fcae31feb2
S.Rasp <s.rasp@physik.uni-muenchen.de>
2016-11-23 11:54:55 +0000

    Added fc_obs_stamps in plot_stamps, first working version

--- plot_stamps.py
+++ plot_stamps.py
@@ -7,11 +7,17 @@ Stephan Rasp
 
 # Imports
 import argparse
-from cosmo_utils.pywgrib import getfobj_ens
+import matplotlib
+matplotlib.use("Agg")
+from cosmo_utils.pywgrib import getfobj_ens, getfobj
+from cosmo_utils.pyncdf import getfobj_ncdf
 from cosmo_utils.plot import ax_contourf
+from cosmo_utils.diag import mean_spread_fieldobjlist
+from cosmo_utils.helpers import yyyymmddhhmmss_strtotime, ddhhmmss_strtotime, yymmddhhmm
 import matplotlib.pyplot as plt
 import numpy as np
 import os
+from datetime import timedelta
 
 # Arguments
 parser = argparse.ArgumentParser(description = 'Process input')
@@ -27,6 +33,11 @@ datadir = '/e/uwork/extsrasp/cosmo_letkf/data_forecast/20160606_00_12_'   # TODO
 precsuf = '_15'
 gribpref = 'lfff'
 nens = 20
+
+radardir = '/e/uwork/extsrasp/radolan/'
+radarpref = 'raa01-rw_10000-'
+radarsufx = '-dwd---bin.nc'
+
 cmPrec = ((1    , 1     , 1    ), 
           (0    , 0.627 , 1    ),
           (0.137, 0.235 , 0.98 ),
@@ -40,8 +51,21 @@ levelsPrec = [0, 1, 3, 10, 30, 100.]
 topdir = datadir + args.expid[0] + '/' + args.date[0] + '/'
 gribfn = gribpref + args.time[0] + precsuf
 ensfobjlist = getfobj_ens(topdir, 'sub', mems = nens, gribfn = gribfn, 
-                          dir_prefix = 'ens', fieldn = 'TOT_PREC_S', para = 2)
+                          dir_prefix = 'ens', fieldn = 'PREC_PERHOUR', para = 2)
 
+if 'fc_obs_stamps' in args.plot:
+    # Load deterministic data
+    detfn = topdir + 'det/' + gribfn
+    detfobj = getfobj(detfn, fieldn = 'PREC_PERHOUR')
+    
+    # Load radar data
+    dateobj = (yyyymmddhhmmss_strtotime(args.date[0]) + 
+               ddhhmmss_strtotime(args.time[0]))
+    radardt = timedelta(minutes = 70)   # TODO Is this correct???
+    radardateobj = dateobj - radardt
+    radarfn = radardir + radarpref + yymmddhhmm(radardateobj) + radarsufx
+    radarfobj = getfobj_ncdf(radarfn, fieldn = 'pr', dwdradar = True)
+    
 # Plot what is to be plotted
 if 'ens_stamps' in args.plot:
     print 'Plotting ens_stamps'
@@ -68,4 +92,32 @@ if 'ens_stamps' in args.plot:
     plt.close('all')
     
     
-
+if 'fc_obs_stamps' in args.plot:
+    print 'Plotting fc_obs_stamps'
+    plotdirsub = (plotdir + '20160606_00_12_' + args.expid[0] + '/' + 
+                  args.date[0] + '/fc_obs_stamps/')
+    if not os.path.exists(plotdirsub): os.makedirs(plotdirsub)
+    
+    # Calculate ensemble mean and spread
+    meanfobj, spreadfobj = mean_spread_fieldobjlist(ensfobjlist)
+    
+    fig, axmat = plt.subplots(2, 2, figsize = (8,10))
+    axlist = np.ravel(axmat)
+    
+    tmpfobjlist = [detfobj, radarfobj, meanfobj, spreadfobj]
+    titlelist = ['det', 'radar', 'ens mean', 'ens spread']
+    
+    for i, (ax, fobj, title) in enumerate(zip(axlist, tmpfobjlist, titlelist)):
+        plt.sca(ax)
+        
+        cf, tmp = ax_contourf(ax, fobj, Basemap_drawrivers = False, npars = 0, 
+                              nmers = 0, colors = cmPrec, pllevels = levelsPrec,
+                              sp_title = title)
+    
+    titlestr = args.expid[0] + ' ' + args.date[0] + ' + ' + args.time[0]
+    fig.suptitle(titlestr, fontsize = 18)
+    
+    plt.tight_layout(rect=[0, 0.0, 1, 0.95])
+    plotstr = str(args.time[0])
+    plt.savefig(plotdirsub + plotstr, dpi = 300)
+    plt.close('all')
