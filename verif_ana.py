0d85caf7b22a99de080feed793f3195d4bf18268
S.Rasp <s.rasp@physik.uni-muenchen.de>
2017-02-20 10:15:02 +0000

    Added verif_int and composite options for verif_ana.py

--- verif_ana.py
+++ verif_ana.py
@@ -23,16 +23,31 @@ parser.add_argument('--date_ana_start', metavar = 'date_ana_start', type=str,
                     help = 'Start date of verification (yyyymmddhhmmss). In case of SYNOP these determine the start and end of the plot which contains verifications for every hour. In case of TEMP or AIREP these determine the start and end of the averaging period which is then displayed as one plot.')
 parser.add_argument('--date_ana_stop', metavar = 'date_ana_stop', type=str,
                     help = 'End date of verification (yyyymmddhhmmss)')
+parser.add_argument('--ver_int_start', metavar = 'ver_int_start', type=int, 
+                    default = 0, 
+                    help = 'Time of day (h/UTC) when verification starts, including')
+parser.add_argument('--ver_int_stop', metavar = 'ver_int_stop', type=int, 
+                    default = 23, 
+                    help = 'Time of day (h/UTC) when verification stops, including')
 parser.add_argument('--var', metavar = 'var', type=str, default = 'T2M',
                     help = 'Variable to be verified.')
 parser.add_argument('--obs', metavar = 'obs', type=str, default = 'SYNOP',
                     help = 'Observation type: SYNOP, TEMP or AIREP')
 parser.add_argument('--active', metavar = 'active', type=str, default = 'False',
                     help = 'If True only active observations are used.')
+parser.add_argument('--composite', metavar = 'composite', type=str, 
+                    default = 'False',
+                    help = 'If True diurnal composite is plotted for SYNOP')
+parser.add_argument('--delete_save', metavar = 'delete_save', type=str, 
+                    default = 'False',
+                    help = 'If True delete old save file')
 args = parser.parse_args()
 
 plotstr = (args.obs + '_' + args.var + '_' + args.date_ana_start + '_' + 
-               args.date_ana_stop)
+           args.date_ana_stop + '_' + str(args.ver_int_start).zfill(2) + 
+           '_' + str(args.ver_int_stop).zfill(2))
+if args.composite == 'True':
+    plotstr += '_composite'
 
 tstart = yyyymmddhhmmss_strtotime(args.date_ana_start)
 tend = yyyymmddhhmmss_strtotime(args.date_ana_stop)
@@ -41,6 +56,14 @@ if tstart == tend:
     timelist = [tstart]
 else:
     timelist = make_timelist(tstart, tend, tint)
+    
+# Filter out times which are not in ver_int 
+new_timelist = []
+for t in timelist:
+    if t.hour >= args.ver_int_start and t.hour <= args.ver_int_stop:
+        new_timelist.append(t)
+timelist = new_timelist
+
 
 cdict = {'radar':'k',
         'REF':'navy',
@@ -85,17 +108,22 @@ for ie, expid in enumerate(args.expid):
     savedir = '/e/uwork/extsrasp/save/' + expid + '/verif_ana/'
     if not os.path.exists(savedir): os.makedirs(savedir)
     savefn = savedir + plotstr + '.npy'
+    if args.delete_save == 'True':
+        print 'delete old save file:', savefn
+        os.system('rm ' + savefn)
     print 'Try to load pre-saved data:', savefn
     if os.path.exists(savefn):
         print 'Found pre-saved data.'
         mean_spread, mean_bias, rmse = np.load(savefn)
     else:
+        print 'Did not find saved data'
         mean_spread = []
         mean_bias = []
         rmse = []
         spread = []
         bias = []
         lev = []
+        hourlist = []
         for t in timelist:
             # Determine 3hrly storage time
             date_ana = yyyymmddhhmmss(t)
@@ -148,6 +176,7 @@ for ie, expid in enumerate(args.expid):
 
             elif args.obs == 'SYNOP':
                 # Get mean values
+                hourlist.append(t.hour)  # From 0 to 23
                 mean_spread.append(np.mean(ens_spread[active]))
                 mean_bias.append(np.mean(det_bias[active]))
                 rmse.append(np.sqrt(np.mean(det_bias[active]**2)))
@@ -158,6 +187,19 @@ for ie, expid in enumerate(args.expid):
 
         # Plot data for each expid
         if args.obs == 'SYNOP':
+            mean_spread = np.array(mean_spread)
+            mean_bias = np.array(mean_bias)
+            rmse = np.array(rmse)
+            if args.composite == 'True':
+                hour_bins = np.arange(-0.5, 25.5, 1)
+                mean_spread = binned_statistic(hourlist, mean_spread, 
+                                               bins = hour_bins)[0]
+                mean_bias = binned_statistic(hourlist, mean_bias, 
+                                               bins = hour_bins)[0]
+                rmse = binned_statistic(hourlist, rmse, 
+                                               bins = hour_bins)[0]
+                hourlist = binned_statistic(hourlist, hourlist, 
+                                               bins = hour_bins)[0]
             np.save(savefn, (mean_spread, mean_bias, rmse))
 
         if args.obs in ['TEMP', 'AIREP']:
@@ -173,12 +215,12 @@ for ie, expid in enumerate(args.expid):
         
         # Plot data for each expid
     if args.obs == 'SYNOP':
-        ax.plot(range(1, len(timelist)+1), mean_spread, c = cdict[expid], linewidth = 2,
-                linestyle = '--')
-        ax.plot(range(1, len(timelist)+1), rmse, c = cdict[expid], linewidth = 2,
-                linestyle = '-', label = expid)
-        ax.plot(range(1, len(timelist)+1), mean_bias, c = cdict[expid], linewidth = 2,
-                linestyle = ':')
+        ax.plot(range(mean_spread.shape[0]), mean_spread, c = cdict[expid], 
+                linewidth = 2, linestyle = '--')
+        ax.plot(range(mean_spread.shape[0]), rmse, c = cdict[expid], 
+                linewidth = 2, linestyle = '-', label = expid)
+        ax.plot(range(mean_spread.shape[0]), mean_bias, c = cdict[expid], 
+                linewidth = 2, linestyle = ':')
 
     if args.obs in ['TEMP', 'AIREP']:
         axarr[0].plot(rmse, meanlev, c = cdict[expid], linewidth = 2)
@@ -189,8 +231,10 @@ for ie, expid in enumerate(args.expid):
 
 # Finish the plots
 if args.obs == 'SYNOP':
-    ax.set_xlabel('time from ' +  yyyymmddhhmmss(tstart - 
-                                                 timedelta(hours = 1))+ ' [h]')
+    if args.composite == 'True':
+        ax.set_xlabel('Time [UTC/h]')
+    else:
+        ax.set_xlabel('time from ' +  yyyymmddhhmmss(tstart)+ ' [h]')
     ax.set_ylabel('[' + unitdict[args.var] +  ']')
     ax.axhline(0, c = 'gray', zorder = 0.1)
     ax.legend(loc = 0, fontsize = 6)
