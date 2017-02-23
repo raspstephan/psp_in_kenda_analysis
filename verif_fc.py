0210cee1c893191e00bf97d5bbd4e2046037cb1a
S.Rasp <s.rasp@lmu.de>
2017-02-23 15:55:46 +0100

    Small fixes

--- verif_fc.py
+++ verif_fc.py
@@ -40,6 +40,21 @@ parser.add_argument('--hint', metavar = 'hint', type=int, default =24,
                     help = 'Maximum forecast lead time')
 args = parser.parse_args()
 
+# General settings
+if os.getcwd() == '/panfs/e/vol0/extsrasp/dwd_scripts':
+    plotdir = '/e/uwork/extsrasp/plots/'
+    datadir = '/e/uwork/extsrasp/cosmo_letkf/data_forecast/'
+    radardir = '/e/uwork/extsrasp/radolan/'
+    savedir_base = '/e/uwork/extsrasp/save/'
+elif os.getcwd() == '/home/s/S.Rasp/repositories/dwd_scripts':
+    datadir = '/home/cosmo/stephan.rasp/dwd_data/data_forecast/'
+    radardir = '/project/meteo/w2w/A6/radolan/netcdf_cosmo_de/'
+    plotdir = '/home/s/S.Rasp/dwd_plots/plots/'
+    savedir_base = '/home/cosmo/stephan.rasp/dwd_data/save/'
+else: 
+    raise Exception('Working directory not recognized:' + os.getcwd())
+
+
 # Config for experiment
 cdict = {'radar':'k',
              'REF':'navy',
@@ -85,7 +100,7 @@ expid_str = ''
 for ie, expid in enumerate(args.expid):
     print 'expid = ', expid
     expid_str += expid + '_'
-    DATA_DIR='/e/uwork/extsrasp/cosmo_letkf/data_forecast/' + expid
+    DATA_DIR=datadir + expid
     
     
     # Check if saved data is available ATTENTION NOT IMPLEMENTED
@@ -180,7 +195,7 @@ for ie, expid in enumerate(args.expid):
                 linestyle = '-')
         
 # Finish the plots
-plotdir = '/e/uwork/extsrasp/plots/' + expid_str[:-1] + '/verif_fof/'
+plotdir = plotdir + expid_str[:-1] + '/verif_fof/'
 if not os.path.exists(plotdir): os.makedirs(plotdir)
 if args.obs in ['TEMP', 'AIREP']:
     axarr[0].set_xlim(rmselimdict[args.var])
