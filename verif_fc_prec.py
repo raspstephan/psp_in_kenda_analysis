99676160d521cc051db171acdfb10d02cda68ed7
S.Rasp <s.rasp@physik.uni-muenchen.de>
2017-02-23 11:46:37 +0000

    Added det verification in verif_fc_prec with fss and normalized rmse (still produces nans...)

--- verif_fc_prec.py
+++ verif_fc_prec.py
@@ -8,13 +8,16 @@ import argparse
 import os
 import numpy as np
 from datetime import timedelta
-from cosmo_utils.da import fdbkfile
 from cosmo_utils.helpers import yyyymmddhhmmss_strtotime, make_timelist, \
-    yyyymmddhhmmss
+    yyyymmddhhmmss, ddhhmmss, ddhhmmss_strtotime, yymmddhhmm
+from cosmo_utils.pyncdf import getfobj_ncdf
+from cosmo_utils.pywgrib import getfobj_ens, getfobj
 from scipy.stats import binned_statistic
 import matplotlib
 matplotlib.use("Agg")
 import matplotlib.pyplot as plt
+from scipy.signal import convolve2d
+from cosmo_utils.scores.probab import FSS
 
 
 # General settings
@@ -31,14 +34,36 @@ elif os.getcwd() == '/home/s/S.Rasp/repositories/dwd_scripts':
 else: 
     raise Exception('Working directory not recognized:' + os.getcwd())
 
+radarpref = 'raa01-rw_10000-'
+radarsufx = '-dwd---bin.nc'
+precsuf = '_15'
+gribpref = 'lfff'
+# Load radar tot mask
+
+totmask = np.load('./radar_tot_mask.npy')
 
 # Define loading functions
-def load_det(expid, date, t):
-    topdir = datadir + expid + '/' + date + '/'
+def load_det(datadir, date, t):
+    topdir = datadir + '/' + date + '/'
     gribfn = gribpref + t + precsuf
     detfn = topdir + 'det/' + gribfn
     detfobj = getfobj(detfn, fieldn = 'PREC_PERHOUR')
+    # Minus one hour
+    #gribfnm1 = gribpref + ddhhmmss(ddhhmmss_strtotime(t) - 
+                                 #timedelta(hours = 1)) + precsuf
+    #detfnm1 = topdir + 'det/' + gribfnm1
+    #detfobjm1 = getfobj(detfnm1, fieldn = 'TOT_PREC')
+    #detfobj.data = detfobj.data - detfobjm1.data
     return detfobj
+
+def load_radar(date, t):
+    dateobj = (yyyymmddhhmmss_strtotime(date) + 
+            ddhhmmss_strtotime(t))
+    radardt = timedelta(minutes = 10)   # TODO Is this correct???
+    radardateobj = dateobj - radardt
+    radarfn = radardir + radarpref + yymmddhhmm(radardateobj) + radarsufx
+    radarfobj = getfobj_ncdf(radarfn, fieldn = 'pr', dwdradar = True)
+    return radarfobj
 
 
 # Arguments
@@ -58,6 +83,10 @@ parser.add_argument('--hint', metavar = 'hint', type=int, default =24,
 parser.add_argument('--ana', metavar = 'ana', type=str,
                     help = 'Type of analysis to be done [di]')
 args = parser.parse_args()
+
+
+plotstr = (args.ana + '_' + args.date_start + '_' + 
+           args.date_stop)
 
 # Config for experiment
 cdict = {'radar':'k',
@@ -80,6 +109,11 @@ if tstart == tend:
 else:
     timelist = make_timelist(tstart, tend, tint)
 
+n = 21
+kernel = np.ones((n,n))/float((n*n))
+
+# Set up figure
+fig, axarr = plt.subplots(1, 2, figsize = (10, 6))
 
 expid_str = ''
 for ie, expid in enumerate(args.expid):
@@ -87,16 +121,82 @@ for ie, expid in enumerate(args.expid):
     expid_str += expid + '_'
     DATA_DIR = datadir + expid
     
-    if args.ana == 'di':
+    if args.ana == 'det':
         radarmean = []
         detmean = []
         hourlist =[]
+        detrmse = []
+        fsslist = []
     
     for t in timelist:
         print t
-        for h in range(1, hint+1):
+        date = yyyymmddhhmmss(t)
+        for h in range(1, args.hint+1):
+            print 'Hour', h
             hourlist.append(h)
             # Load data 
+            if args.ana == 'det':
+                # Det run 
+                detfobj = load_det(DATA_DIR, date, ddhhmmss(timedelta(hours=h)))
+                radarfobj = load_radar(date, ddhhmmss(timedelta(hours=h)))
+                
+                radarmean.append(np.mean(radarfobj.data[~totmask]))
+                detmean.append(np.mean(detfobj.data[~totmask]))
+                
+                # Upscaled RMSE
+                nanradar = radarfobj.data
+                nandet = detfobj.data
+                nanradar[totmask] = np.nan
+                nandet[totmask] = np.nan 
+                convradar = convolve2d(nanradar, kernel, mode='same')
+                convdet = convolve2d(nandet, kernel, mode='same')
+                detrmse.append(np.sqrt(np.nanmean((convradar - convdet)**2 / 
+                                       (0.5*(convradar + convdet))**2)))
+                fss = FSS(1, 21, nanradar, nandet, python_core = True)
+                fsslist.append(fss)
+        # End hourlist 
+    # End timelist
+    
+    # Bin the data 
+    hour_bins = np.arange(0.5, 25.5, 1)
+    radarmean = binned_statistic(hourlist, radarmean, 
+                                    bins = hour_bins)[0]
+    detmean = binned_statistic(hourlist, detmean, 
+                                    bins = hour_bins)[0]
+    detrmse = binned_statistic(hourlist, detrmse, 
+                                    bins = hour_bins)[0]
+    meanfss = binned_statistic(hourlist, fsslist, 
+                                    bins = hour_bins)[0]
+    
+    # Plot
+    axarr[0].plot(range(radarmean.shape[0]), radarmean, c = 'k', linewidth = 2)
+    axarr[0].plot(range(radarmean.shape[0]), detmean, c = cdict[expid], 
+                  linewidth = 2,
+                  label = expid)
+    axarr[1].plot(range(radarmean.shape[0]), detrmse, c = cdict[expid], 
+                  linewidth = 2)
+    axarr[1].plot(range(radarmean.shape[0]), meanfss, c = cdict[expid], 
+                  linewidth = 2, linestyle = '--')
+
+# End expid loop
+# Finish the plots
+
+axarr[0].set_xlabel('time [UTC/h]')
+axarr[1].set_xlabel('time [UTC/h]')
+axarr[0].set_label('[mm/h]')
+axarr[1].set_label('FSS')
+axarr[0].set_title('domain mean precipitation')
+axarr[1].set_title('FSS n = ' + str(n*2.8) + 'km')
+axarr[0].legend(loc = 0, fontsize = 6)
+plt.tight_layout(rect=[0, 0.0, 1, 0.95])
+
+plotdir = plotdir + expid_str[:-1] + '/verif_fc_prec/'
+if not os.path.exists(plotdir): os.makedirs(plotdir)
+fig.suptitle(expid_str[:-1] + '  ' + plotstr)
+print 'Save figure:', plotdir + plotstr
+fig.savefig(plotdir + plotstr, dpi = 300)
+plt.close('all')
+
             
         
     
