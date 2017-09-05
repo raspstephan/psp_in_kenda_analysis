Fixed ddof error

helpers.py
@@ -147,7 +147,7 @@ def set_plot(ax, title, args, hourlist_plot):
 
 
 def compute_ens_stats(convradar, convfieldlist, ens_norm_type,
-                      norm_thresh = 0.1):
+                      norm_thresh=0.1):
     """
     Compute spread and rmse of ensemble with given normalization type.
     0: no normalization
@@ -157,16 +157,17 @@ def compute_ens_stats(convradar, convfieldlist, ens_norm_type,
     meanfield = np.mean(convfieldlist, axis=0)
     ensradarmean = 0.5 * (convradar + meanfield)
     if ens_norm_type == 0:  # No normalization
-        spread = np.nanmean(np.std(convfieldlist, axis=0))
+        spread = np.nanmean(np.std(convfieldlist, axis=0, ddof=1))
         rmse = np.sqrt(np.nanmean((convradar - meanfield) ** 2))
     elif ens_norm_type == 1:   # Grid point normalization
-        spread = np.nanmean((np.std(convfieldlist, axis=0) /
+        spread = np.nanmean((np.std(convfieldlist, axis=0, ddof=1) /
                              meanfield)[meanfield >= norm_thresh])
         rmse = np.sqrt(np.nanmean(((convradar - meanfield) ** 2 /
                                    ensradarmean ** 2)[ensradarmean >= 0.1]))
 
     elif ens_norm_type == 2:   # Domain normalization
-        spread = (np.nanmean(np.std(convfieldlist, axis=0)[meanfield >= 0.1]) /
+        spread = (np.nanmean(np.std(convfieldlist, axis=0, ddof=1)
+                             [meanfield >= 0.1]) /
                   np.nanmean(meanfield[meanfield >= 0.1]))
         rmse = (np.sqrt(np.nanmean(((convradar - meanfield) ** 2)
                                     [ensradarmean >= 0.1])) /

verif_ana_prec.py
@@ -175,7 +175,7 @@ aspect = 0.75
 if args.composite:
     x = np.unique(hourlist)
     plotstr = ('comp_' + args.date_ana_start + '_' + args.date_ana_stop +
-               '_n_' + str(args.n_kernel))
+               '_n_' + str(args.n_kernel) + '_norm_' + str(args.ens_norm_type))
     fig1, ax1 = plt.subplots(1, 1, figsize=(pw / 2., pw / 2. * aspect))
     fig2, ax2 = plt.subplots(1, 1, figsize=(pw / 2., pw / 2. * aspect))
     for ie, expid in enumerate(args.expid):
@@ -214,7 +214,8 @@ else:
         daylist = [tstart]
     for iday, day in enumerate(daylist):
         date = yyyymmddhhmmss(day)
-        plotstr = date + '_n_' + str(args.n_kernel)
+        plotstr = (date + '_n_' + str(args.n_kernel) + '_norm_' +
+                   str(args.ens_norm_type))
         fig1, ax1 = plt.subplots(1, 1, figsize=(pw / 2., pw / 2. * aspect))
         fig2, ax2 = plt.subplots(1, 1, figsize=(pw / 2., pw / 2. * aspect))
         for ie, expid in enumerate(args.expid):